20405c7c296171000c8d2bb913044af9fb96e947
vivekrahul-91 <vivek.rahul@astrotalk.com>
2025-12-15 14:14:20 +0530

    first commit

---
+++ assets/payment-button.js
@@ -0,0 +1,13 @@
+if (!window.Eurus.loadedScript.includes('payment-button.js')) {
+  window.Eurus.loadedScript.push('payment-button.js');
+
+  document.addEventListener('alpine:init', () => {
+      Alpine.store('xShopifyPaymentBtn', {
+        load(e) {
+          if (Shopify && Shopify.PaymentButton) {
+            Shopify.PaymentButton.init();
+          }
+        },
+      });
+    });
+}
